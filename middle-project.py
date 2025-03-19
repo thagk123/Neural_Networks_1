5b728f5c75378882dbe573bf607b1ed4f7c925ed
thagk123 <gkoutisa@csd.auth.gr>
2025-03-19 10:01:59 +0200

    change

--- middle-project.py
+++ middle-project.py
@@ -10,9 +10,10 @@ start_time = time.time()
 
 # Συνάρτηση για αποσειριοποίηση
 def unpickle(file):
+    """Loads and returns a dictionary from a pickle file."""
     with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+        data = pickle.load(fo, encoding='bytes')  # Αποφυγή σύγκρουσης με το built-in dict
+    return data
 
 # Συνάρτηση για τη φόρτωση της CIFAR-10
 def load_cifar10_data(folder_path):
